b2e2d2e21f8b0f41c1b89a0b6e2c6de36708e54d
kaw393939 <keith@webizly.com>
2019-01-18 13:55:50 -0500

    Task: Cleanup

--- routes/routes.php
+++ routes/routes.php
@@ -13,22 +13,4 @@ Route::group(array('middleware' => ['bindings','api'], 'prefix' => 'api', 'names
     Route::get('groups/mygroups','GroupsController@mygroups')->name('groups.mygroups');
     Route::apiResource('groups','GroupsController');
     Route::apiResource('groups/{group}/users','GroupUsersController');
-
-    // Route::apiResource('groups/{group}/members','GroupUsersController');
-
-   // Route::apiResource('groups/{group}/manage/users', 'GroupOwnerController');
-
-    /*
-    Route::post('groups/{group}/users/subscribe','subscribe@GroupUsersController');
-    Route::post('groups/{group}/users/unsubscribe','unsubscribe@GroupUsersController');
-    Route::post('groups/{group}/users/{user}/promote','promote@GroupUsersController');
-    Route::post('groups/{group}/users/{user}/demote','demote@GroupUsersController');
-    */
-
 });
-
-/*
-Route::group(array('middleware' => ['bindings','api'], 'prefix' => 'api', 'namespace' => '\Kaw393939\Group\Http\Controllers'), function()
-{
-});
-*/
